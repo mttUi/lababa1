45293b3c30169a8d6c39b7d94f608e03888995ab
mttUi <158287701+mttUi@users.noreply.github.com>
2024-05-20 18:37:18 +0200

    creating res1 with lab1

---
+++ Res1/lab1/lab1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+//Целочисленные типы 
+//short - 2 байта, -2^15...2^15-1
+//int - 4 байта, -2^31...2^31-1
+//long - 8 байт, -2^31...2^31-1
+//long long - 8 байт, -2^63...2^63-1 
+
+//Вещественные типы
+//float - 4 байта, 7 значащих цифр
+//double - 8 байт, 15 значащих цифр
+//long double -  тоже что и double
+
+using namespace std;
+
+int main() {
+    double h, S;
+    long double a;
+    cout << "S= "; cin >> S;
+    cout << "h= "; cin >> h;
+    a = (2 * S) / h;
+    cout << "a= " << a << endl;
+    return 0;
+}
